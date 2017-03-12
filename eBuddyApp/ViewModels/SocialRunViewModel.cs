94ddd5cb32dc912fc7968f840229f11759339d82
gal.klm@gmail.com <gosdev1!>
2017-03-12 19:33:53 +0200

    change to time

--- eBuddyApp/ViewModels/SocialRunViewModel.cs
+++ eBuddyApp/ViewModels/SocialRunViewModel.cs
@@ -24,24 +24,36 @@ namespace eBuddyApp.ViewModels
         private Geopoint _BuddyLocation;
         public Geopoint BuddyLocation { get { return _BuddyLocation; } private set { Set(ref _BuddyLocation, value); } }
 
-        public override RunItem RunData {
+        public override RunItem RunData
+        {
             get { return BuddyRunManager.Instance.RunData; }
         }
 
         public SocialRunViewModel()
         {
-            StartRun = new RelayCommand(() => { BuddyRunManager.Instance.Start(); },
-                () => { return (!BuddyRunManager.Instance.InRun /*&& BandService.Instance.IsConnected*/); });
+           
+            StartRun = new RelayCommand(() =>
+            {
+                BuddyRunManager.Instance.Start();
+                StopRun.RaiseCanExecuteChanged();
+                StartRun.RaiseCanExecuteChanged();
+            },
+    () => { return (!BuddyRunManager.Instance.InRun /*&& BandService.Instance.IsConnected*/); });
 
-            StopRun = new RelayCommand(() => BuddyRunManager.Instance.Stop(),
+            StopRun = new RelayCommand(() =>
+            {
+                BuddyRunManager.Instance.Stop();
+                StopRun.RaiseCanExecuteChanged();
+                StartRun.RaiseCanExecuteChanged();
+            },
                 () => { return BuddyRunManager.Instance.InRun; });
 
+            BuddyRunManager.Instance.OnRouteUpdate += Instance_OnRouteUpdate;
             LocationService.Instance.OnLocationChange += Instance_OnLocationChange;
             BandService.Instance.OnHeartRateChange += Instance_OnHeartRateChange;
-
             CurrentLocation = ExtentionMethods.GetDefaultPoint();
             BuddyRunManager.Instance.OnBuddyRouteUpdate += Instance_OnBuddyRouteUpdate;
-  
+
         }
 
         private void Instance_OnBuddyRouteUpdate(MapRoute e)
